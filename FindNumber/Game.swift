f34ea1ca307b912da4df88d6ff2e3cffa904c16f
Dmitiy <dimapermyakov55@gmail.com>
2023-01-29 07:26:48 +0300

    Finished the game

--- FindNumber/Game.swift
+++ FindNumber/Game.swift
@@ -12,40 +12,87 @@ enum StatusGame {
     case lose
     case start
 }
+
 class Game {
     struct Item {
         var title: String
         var isFound: Bool = false
+        var isError: Bool = false
     }
     
     var items = [Item]()
     private var countItems: Int
     var nextItem: Item?
-    var status: StatusGame = .start
-    init(countItems: Int) {
+    var status: StatusGame = .start {
+        didSet {
+            if status != .start {
+                stopGame()
+            }
+        }
+    }
+    private var timeForGame: Int
+    private var secondsGame: Int {
+        didSet {
+            if secondsGame == 0 {
+                status = .lose
+                
+            }
+            updateTimer(status, secondsGame)
+        }
+    }
+    private var timer: Timer?
+    private var updateTimer: ((StatusGame, Int) -> Void)
+    
+    
+    init(countItems: Int, timerForGame time: Int, updateTimer: @escaping (_ status: StatusGame, _ seconds: Int) -> Void) {
         self.countItems = countItems
+        self.secondsGame = time
+        self.updateTimer = updateTimer
+        self.timeForGame = time
         setupGame()
     }
     
+    
     private func setupGame() {
         var digits = Array(1...99).shuffled()
+        items.removeAll()
         while items.count < countItems {
             let item = Item(title: String(digits.removeFirst()))
             items.append(item)
         }
         // Загадываем число для поиска.
         nextItem = items.shuffled().first
+        updateTimer(status, secondsGame)
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak self] (_) in
+            self?.secondsGame -= 1
+        })
     }
     
+    
     func check(index: Int) {
+        guard status == .start else { return }
+        
         if items[index].title == nextItem?.title {
             items[index].isFound = true
             nextItem = items.shuffled().first(where: {$0.isFound == false})
+        } else {
+            items[index].isError = true
         }
         
         if nextItem == nil {
             status = .win
         }
+    }
+    
+    
+    func newGame() {
+        status = .start
+        self.secondsGame = timeForGame
+        setupGame()
+    }
+    
+    private func stopGame() {
+        timer?.invalidate()
     }
     
 }
--- FindNumber/GameViewController.swift
+++ FindNumber/GameViewController.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var newGameButton: UIButton!
     @IBOutlet var buttons: [UIButton]! // Коллекция кнопок.
-    lazy var game = Game(countItems: buttons.count)
+    @IBOutlet weak var timerLabel: UILabel!
+    lazy var game = Game(countItems: buttons.count, timerForGame: 30) { [weak self] (status, time) in
+        guard let self = self else { return }
+        self.timerLabel.text = time.textForTimer()
+        self.updateInfoGame(with: status)
+    }
     @IBOutlet weak var nextDigit: UILabel! // Загадывание числа для поиска.
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,21 +36,67 @@ class GameViewController: UIViewController {
 
     }
     
+    @IBAction func pressNewGame(_ sender: UIButton) {
+        game.newGame()
+        sender.isHidden = true
+        setupScreen()
+    }
     
     func setupScreen() {
     /* Функция создаёт кнопки с рандомными числами. */
         
         for i in game.items.indices {
             buttons[i].setTitle(game.items[i].title, for: .normal)
+            buttons[i].isHidden = false
         }
         nextDigit.text = game.nextItem?.title
     }
     
-    func updateUI() {
+    
+    private func updateUI() {
         /* Скрываем кнопку */
         for i in game.items.indices {
             buttons[i].isHidden = game.items[i].isFound
+            if game.items[i].isError {
+                UIView.animate(withDuration: 0.3) { [weak self] in
+                    self?.buttons[i].backgroundColor = .red
+                } completion: { [weak self] (_) in
+                    self?.buttons[i].backgroundColor = .white
+                    self?.game.items[i].isError = false
+                }
+            }
         }
         nextDigit.text = game.nextItem?.title
+        
+        updateInfoGame(with: game.status)
+    }
+    
+    
+    private func updateInfoGame(with status: StatusGame) {
+        switch status {
+        case .start:
+            statusLabel.text = "Игра началась"
+            statusLabel.textColor = .black
+            newGameButton.isHidden = true
+        case .win:
+            statusLabel.text = "Вы победили"
+            statusLabel.textColor = .green
+            newGameButton.isHidden = false
+        case .lose:
+            statusLabel.text = "Вы проиграли!"
+            statusLabel.textColor = .red
+            newGameButton.isHidden = false
+        }
+    }
+}
+
+extension Int {
+    func textForTimer() -> String {
+        let minutes = self / 60
+        let seconds = self % 60
+        if minutes < 10 {
+            return String(format: "0%d:%02d", minutes, seconds)
+        }
+        return String(format: "%d:%02d", minutes, seconds)
     }
 }
